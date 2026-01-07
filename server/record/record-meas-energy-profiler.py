510eec44f691b93872e6a0432c15aee94f2bc7c1
Gilles Callebaut, PhD <callebaut.gilles@gmail.com>
2026-01-07 14:17:56 +0100

    update meas

--- server/record/record-meas-energy-profiler.py
+++ server/record/record-meas-energy-profiler.py
@@ -21,7 +21,7 @@ import zmq
 # ****************************************************************************************** #
 
 SAVE_EVERY = 60.0  # seconds
-FOLDER = "RECI-0"  # subfolder inside data/ where to save measurement data
+FOLDER = "sionna0-0"  # subfolder inside data/ where to save measurement data
 TIMESTAMP = round(time())
 DEFAULT_DURATION = None  # seconds, override via CLI
 
