a053758d41411a37b00f21facfa29c5c7e8a498d
Agata872 <2310123801@qq.com>
2026-01-27 17:54:08 +0000

    更新客户端脚本名称为 run_qpsk.py，并修改数据保存文件夹名称为 ZF-test1

--- server/record/record-meas-scope.py
+++ server/record/record-meas-scope.py
@@ -27,7 +27,7 @@ import zmq
 # ****************************************************************************************** #
 
 SAVE_EVERY = 60.0  # seconds
-FOLDER = "ZF-test"  # subfolder inside data/where to save measurement data
+FOLDER = "ZF-test1"  # subfolder inside data/where to save measurement data
 TIMESTAMP = round(time())
 DEFAULT_DURATION = None  # seconds, override via CLI
 
